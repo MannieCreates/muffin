f96f24774c6d4b92e0cdbb6e879f62a48eb13dcc
Khondker Arnob <k.arnob9@gmail.com>
2024-02-22 16:50:12 +0600

    First commit - Commiting deployable template to server

---
+++ alpha/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+# Explicitly declare app_label
+class Meta:
+    app_label = 'alpha'
---
+++ alpha/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Item
+
+def index(request):
+    items = Item.objects.all()
+    return render(request, 'index.html', {'items': items})
+
+# Create your views here.
