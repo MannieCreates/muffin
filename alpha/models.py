44afc616cd8681779bc673371b85021f217feb1a
Khondker Arnob <k.arnob9@gmail.com>
2024-02-25 00:49:14 +0600

    Database and table added : onek_pera -> login_info(username*, password, age, name, email, phone_number)

--- alpha/models.py
+++ alpha/models.py
@@ -6,6 +6,14 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+class login_info(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=128)
+    age = models.IntegerField()
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    # birth_date = models.DateField()
 
 # Explicitly declare app_label
 class Meta:
--- emalle/settings.py
+++ emalle/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'emalle.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'onek_pera',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
